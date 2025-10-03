ce54e343a596bdbf4ef41e227a52f078d7b95f9e
Rhaissa de Lima Santos <824133063@ulife.com.br>
2025-10-03 11:54:43 -0300

    Nova Branch

--- order-service/orderService.js
+++ order-service/orderService.js
@@ -1,13 +1,69 @@
-const express = require('express');
+require ('dotenv').config();
+const express = require ('express');
+const mongoose = require ('mongoose');_
+
+
 const app = express();
-app.use(express.json());
+app.use(express.json());                        
 
-app.post('/pedidos', (req, res) => {
-    const pedido = req.body;
-    console.log(`Pedido recebido par a o usuario Id: ${pedido.usuarioId}`);
-    res.send({message: 'Pedido criado com sucesso!', pedido})
-})
+const Mongo_Uri = process.env.MONGO_URI; 
+if (!Mongo_Uri) {
+    throw new Error('MONGO_URI is not defined in environment variables');
+}
 
-app.listen(4000, () => {
-    console.log('Order Service rodando na porta 4000');
+mongoose.connect(Mongo_Uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+const orderSchema = new mongoose.Schema({
+    userId:{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    items: {type: [String], dafault: []},
+    total: {type: Number, default: 0},
+    createdAt: {type: Date, default: Date.now},
 });
+
+const Order = mongoode.model('Order', orderSchema);
+
+app.post('/pedidos', async (req, res) => {
+    try {
+        const {userId, items= [], total = 0} = req.body;
+        if (!userId) 
+            return res.status(400).json({error: 'userId is required'});
+
+            const order = new Order({userId, items, total});
+            const saved = await order.save();
+            console.log(`Pedido criado: ${saved_id} para user ${userId}`) 
+            res.status(201).json(saved);
+            } catch (error) {
+                console.error('Error creating order:', error);
+                res.status(500).json({error: 'Erro ao criar server pedido'});
+            }
+         
+        }
+    );
+
+
+app.get('pedidos', async (req, res) => {
+    try {
+        const orders = await Order.find().sort({createdAt: -1});
+
+        return res.json(orders);
+    }catch (err){
+        console.error(err);
+        return res.status(500).json({error: 'Erro ao buscar pedidos'});
+    }
+});
+        app.listen (4000, () => {
+            console.log('Order service running on port 4000');
+        
+});
+
+
+
+
+
+
+
+
